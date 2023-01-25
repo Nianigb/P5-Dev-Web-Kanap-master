683437d2c918584ff15f51e8f85085918b431c2f
Nianigb <guerrab.anissa@gmail.com>
2023-01-25 11:48:55 +0100

    Modif validation commande + correction panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -50,7 +50,6 @@ async function infosproduit() {
     const cart__items = document.querySelector("#cart__items");
     cart__items.innerHTML = produit;
 
-    modifyQuantity();
     quantite = totalQuantity();
     // Mettre à jour la quantité totale sur la page
     document.getElementById("totalQuantity").innerHTML = quantite;
@@ -72,31 +71,6 @@ async function infosproduit() {
     }
   }
 
-  function modifyQuantity() {
-    // Fonction ajouter ou retirer un article du panier en cours
-    var modifyQuantity = document.querySelectorAll(".itemQuantity");
-
-    modifyQuantity.forEach((modify) => {
-      modify.addEventListener('change', (e) => {
-        let ciblearticle = modify.closest("article");
-        let recupid = ciblearticle.getAttribute("data-id");
-        let recupcouleur = ciblearticle.getAttribute("data-color");
-        let newquantity = parseInt(e.target.value);
-        let searchproduct = tableaupanier.find(a => a.id == recupid && a.couleur == recupcouleur);
-        // Nouvelle quantité de l'article
-        searchproduct.quantite = newquantity;
-        if (searchproduct.quantite > 0 && searchproduct.quantite <= 100) {
-          // Mise à jour du panier
-          savePanier(tableaupanier);
-          location.reload();
-          // tableaupanier = tableaupanier.filter(a => a.id != recupid || a.couleur != recupcouleur);
-        }
-
-      }
-      )
-    })
-  }
-
   // Fonction qui va retirer un produit du panier en cours
   var deleteItem = document.querySelectorAll(".deleteItem");
 
@@ -112,179 +86,132 @@ async function infosproduit() {
       location.reload();
     })
   })
-}
 
-// Fonction pour sauvegarder le panier
-function savePanier(produits) {
-  // Mise à jour du panier dans localstorage
-  localStorage.setItem('panier', JSON.stringify(produits));
-}
+  let addQuantite = document.querySelectorAll('.itemQuantity'); // Cible l'élément ainsi que ca valeur
+  for (let add of addQuantite) {
+    add.addEventListener('click', () => {
+      let article = add.closest("article");
+      let dataId = article.getAttribute('data-id');
+      let dataCouleur = article.getAttribute('data-color');
+      for (let local of tableaupanier) {
+        if ((local.id === dataId) && (local.couleur === dataCouleur)) {
+          let newQuantite = parseInt(add.value);
+          local.quantite = newQuantite;
+          break;
+        }
+      }
+      localStorage.setItem("panier", JSON.stringify(tableaupanier));
+      window.location.reload();
+    })
 
-// Formulaire client
-const commander = document.getElementById("order");
-// Variables récupérées pour le formulaire :
-const prenom = document.getElementById("firstName");
-const nom = document.getElementById("lastName");
-const adresse = document.getElementById("address");
-const ville = document.getElementById("city");
-const mail = document.getElementById("email");
+  }
 
-// Etapes du formulaire de commande
-// Première vérification
-let firstname = false;
-prenom.addEventListener('change', (e) => {
-  // Vérification du prénom
-  firstname = firstNameValid(prenom.value);
-  const firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
-  if (firstname == false) {
-    firstNameErrorMsg.innerHTML = "Vérifier le prénom";
-  }
-  else {
-    firstNameErrorMsg.innerHTML = " ";
-  }
-})
-
-//Deuxième vérification
-let lastname = false;
-nom.addEventListener('change', (e) => {
-  // Vérification du nom
-  lastname = lastNameValid(nom.value);
-  const lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
-  if (lastname == false) {
-    lastNameErrorMsg.innerHTML = "Vérifier le nom";
-  }
-  else {
-    lastNameErrorMsg.innerHTML = " ";
-  }
-})
-
-//Troisième vérification
-let adress = false;
-adresse.addEventListener('change', (e) => {
-  // Vérification de l'adresse
-  adress = adressValid(adresse.value);
-  const adressErrorMsg = document.getElementById("addressErrorMsg");
-  if (adress == false) {
-    adressErrorMsg.innerHTML = "Vérifier l'adresse";
-  }
-  else {
-    adressErrorMsg.innerHTML = " ";
-  }
-})
-
-//Quatrième vérification
-let city = false;
-ville.addEventListener('change', (e) => {
-  // Vérification de la ville
-  city = cityValid(ville.value);
-  const cityErrorMsg = document.getElementById("cityErrorMsg");
-  if (city == false) {
-    cityErrorMsg.innerHTML = "Vérifier la ville";
-  }
-  else {
-    cityErrorMsg.innerHTML = " ";
-  }
-})
-
-//Cinquième vérification
-let email = false;
-mail.addEventListener('change', (e) => {
-  // Vérification de l'email
-  email = emailValid(mail.value);
-  const mailErrorMsg = document.getElementById("emailErrorMsg");
-  if (email == false) {
-    mailErrorMsg.innerHTML = "Vérifier l'email";
-  }
-  else {
-    mailErrorMsg.innerHTML = " ";
-  }
-})
+  document.getElementById("totalPrice").innerHTML = prix;
 
 
-const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
-
-// Test du first name
-function firstNameValid(prenom) {
-  if (regexName.test(prenom) == false) {
-    return false;
-  } else {
-    return true;
+  // Fonction pour sauvegarder le panier
+  function savePanier(produits) {
+    // Mise à jour du panier dans localstorage
+    localStorage.setItem('panier', JSON.stringify(produits));
   }
-}
 
-// Test du last name
-function lastNameValid(nom) {
-  if (regexName.test(nom) == false) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-// Test de l'adresse
-function adressValid(adresse) {
-  const regexAdress = /^([A-Za-z0-9]+( [A-Za-z0-9]+)+).*$/i;
-  if (regexAdress.test(adresse) == false) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-// Test de la ville
-function cityValid(ville) {
-  if (regexName.test(ville) == false) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-// Test de l'email
-function emailValid(mail) {
-  const regexMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (regexMail.test(mail) == false) {
-    return false;
-  } else {
-    return true;
-  }
-}
-// Au moment du clic pour commander,
-// permet de retrouver le numéro de commande et de rediriger vers une page de confirmation
-commander.addEventListener('click', () => {
-  passerCommande();
-
-})
-async function passerCommande() {
-  if (firstname && lastname && adress && city && email) {
-    let contact = {
-      firstName: document.querySelector("#firstName").value,
-      lastName: document.querySelector("#lastName").value,
-      address: document.querySelector("#address").value,
-      city: document.querySelector("#city").value,
-      email: document.querySelector("#email").value,
-    };
-    // Tableau qui contient les id du local storage
-    let products = [];
-    for (let eltpanier of tableaupanier) {
-      products.push(eltpanier.id);
+  // Fonction qui récupère tous les élements du formulaire et passe les étapes de vérifications
+  function verifierChamp(champ) {
+    if (champ.value === "") {
+      document.getElementById(`${champ.id}ErrorMsg`).innerHTML = `Veuillez entrer votre ${champ.id}.`;
+      return false;
+    } else if (!champ.regex.test(champ.value)) {
+      document.getElementById(`${champ.id}ErrorMsg`).innerHTML = champ.errorMessage;
+      return false;
+    } else {
+      document.getElementById(`${champ.id}ErrorMsg`).innerHTML = "";
+      return true;
     }
+  }
+  // La fonction "verifierForm" est utilisée pour valider le formulaire en vérifiant si tous les champs 
+  // sont remplis correctement en appelant la fonction "verifierChamp" pour chaque champ.
+  function verifierForm() {
+    let champs = [
+      {
+        id: "firstName",
+        value: document.getElementById("firstName").value,
+        regex: /^([^0-9]*)$/,
+        errorMessage: "Veuillez entrer un prénom valide."
+      },
+      {
+        id: "lastName",
+        value: document.getElementById("lastName").value,
+        regex: /[a-zA-Z]/,
+        errorMessage: "Veuillez entrer un nom valide."
+      },
+      {
+        id: "address",
+        value: document.getElementById("address").value,
+        regex: /^[a-zA-Z0-9\s,'-]*$/,
+        errorMessage: "Veuillez entrer une adresse valide."
+      },
+      {
+        id: "city",
+        value: document.getElementById("city").value,
+        regex: /[a-zA-Z]/,
+        errorMessage: "Veuillez entrer une ville valide."
+      },
+      {
+        id: "email",
+        value: document.getElementById("email").value,
+        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        errorMessage: "Veuillez entrer un email valide."
+      }
+    ];
+    for (let champ of champs) {
+      if (!verifierChamp(champ)) {
+        return false;
+      }
+    }
+    return true
+  }
+
+  // La fonction "creerCommande" récupère les informations du client dans le formulaire
+  // et les envoie au serveur pour créer une commande.
+  async function creerCommande() {
+    if (verifierForm() === false) {
+      return
+    }
+
+    let contact = {
+      firstName: document.getElementById("firstName").value,
+      lastName: document.getElementById("lastName").value,
+      address: document.getElementById("address").value,
+      city: document.getElementById("city").value,
+      email: document.getElementById("email").value,
+    }
+
+    let detailsPanier = {
+      products: tableaupanier.map(product => (product.id)), contact
+    };
 
     try {
       const response = await fetch('http://localhost:3000/api/products/order', {
         method: 'POST',
-        body: JSON.stringify({products, contact}),
+        body: JSON.stringify(detailsPanier),
         headers: {
           'Content-Type': 'application/json'
         }
       });
       const data = await response.json();
       location.href = `confirmation.html?orderId=${data.orderId}`;
-      localStorage.clear();
+      if (localStorage) {
+        localStorage.clear();
+      }
     } catch (error) {
       console.error(error);
     }
   }
 
+  let boutonCommander = document.getElementById('order');
 
+  boutonCommander.addEventListener('click', (event) => {
+    event.preventDefault();
+    creerCommande();
+  });
 }
